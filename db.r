a5e2d9679bbea6e313ef1530666535f2051e15ed
victor <victordj587@gmail.com>
2024-08-16 15:02:51 +0200

    first commit

---
+++ db.r
@@ -0,0 +1,31 @@
+library(dotenv)
+library(RPostgreSQL)
+
+load_dot_env(file = ".env")
+
+dsn_database <- Sys.getenv("DB_NAME")
+dsn_hostname <- Sys.getenv("DB_HOST")
+dsn_port <- Sys.getenv("DB_PORT")
+dsn_uid <- Sys.getenv("DB_UID")
+dsn_pwd <- Sys.getenv("DB_PASS")
+
+# Create a connection to the database
+tryCatch(
+  {
+    drv <- dbDriver("PostgreSQL")
+    print("Connecting to Database…")
+    connec <- dbConnect(
+      drv,
+      dbname = dsn_database,
+      host = dsn_hostname,
+      port = dsn_port,
+      user = dsn_uid,
+      password = dsn_pwd
+    )
+    print("Database Connected!")
+  },
+  error = function(cond) {
+    print("Unable to connect to Database.")
+    print(cond)
+  }
+)
